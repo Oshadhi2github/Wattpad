7629ad75be1009517411cd00623de6f0a04e2818
Oshadhi2github <pabasarasgck@gmail.com>
2022-11-12 10:33:45 +0530

    set date format for dashboard form

--- src/com/opk/pos/controller/DashBoardFormController.java
+++ src/com/opk/pos/controller/DashBoardFormController.java
@@ -1,4 +1,27 @@
 package com.opk.pos.controller;
 
+import javafx.scene.control.Label;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DashBoardFormController {
+    public Label lblDate;
+    public Label lblTime;
+
+    public void initialize(){
+        setDateAndTime();
+    }
+
+    private void setDateAndTime() {
+        //set date
+       /* Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
+        String formattedDate = dateFormat.format(date);
+        lblDate.setText(formattedDate);*/
+
+        lblDate.setText(new SimpleDateFormat("YYYY-MM-dd").format(new Date()));
+
+
+    }
 }
