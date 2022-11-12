ab93ec96224b78e3796af26ead3c06de3b55b6cd
Oshadhi2github <pabasarasgck@gmail.com>
2022-11-12 11:34:15 +0530

    set time format for dashboard form

--- src/com/opk/pos/controller/DashBoardFormController.java
+++ src/com/opk/pos/controller/DashBoardFormController.java
@@ -1,8 +1,16 @@
 package com.opk.pos.controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DashBoardFormController {
@@ -21,6 +29,18 @@ public class DashBoardFormController {
         lblDate.setText(formattedDate);*/
 
         lblDate.setText(new SimpleDateFormat("YYYY-MM-dd").format(new Date()));
+
+        //===========================
+        //set time
+        final DateFormat format = DateFormat.getDateInstance();
+        Timeline timeline = new Timeline(new KeyFrame(Duration.ZERO, event -> {
+            /*Calendar cal = Calendar.getInstance();
+            lblTime.setText(format.format(cal.getTime()));*/
+            lblTime.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+
+        }),new KeyFrame(Duration.seconds(1)));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
 
 
     }
