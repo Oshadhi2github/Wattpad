db3c4da5f475e8f0b5a19c4583e3ceb28adf9c2e
Oshadhi2github <pabasarasgck@gmail.com>
2022-11-12 14:14:45 +0530

    save customer data

--- src/com/opk/pos/controller/CustomerFormController.java
+++ src/com/opk/pos/controller/CustomerFormController.java
@@ -1,4 +1,45 @@
 package com.opk.pos.controller;
 
+import com.opk.pos.db.Database;
+import com.opk.pos.model.Customer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CustomerFormController {
+    public AnchorPane customerFormContext;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DashBoardForm","Dashboard");
+    }
+
+    private void setUi(String location, String title) throws IOException {
+        Stage window = (Stage)customerFormContext.getScene().getWindow();
+        window.setTitle(title);
+        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+    }
+
+    public void saveUpdateOnAction(ActionEvent actionEvent) {
+        Customer customer = new Customer(
+                txtId.getText(),
+                txtName.getText(),
+                txtAddress.getText(),
+                Double.parseDouble(txtSalary.getText())
+        );
+        if (Database.customerTable.add(customer)){
+            new Alert(Alert.AlertType.CONFIRMATION,"Customer Saved!").show();
+        }else{
+            new Alert(Alert.AlertType.CONFIRMATION,"Try Again!").show();
+        }
+    }
 }
--- src/com/opk/pos/db/Database.java
+++ src/com/opk/pos/db/Database.java
@@ -1,9 +1,11 @@
 package com.opk.pos.db;
 
+import com.opk.pos.model.Customer;
 import com.opk.pos.model.User;
 
 import java.util.ArrayList;
 
 public class Database {
     public static ArrayList<User> userTable = new ArrayList<User>();
+    public static ArrayList<Customer> customerTable = new ArrayList<Customer>();
 }
