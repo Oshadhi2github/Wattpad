eb0251dcb4306392fefd3070ce3c585aaf32885a
Oshadhi2github <pabasarasgck@gmail.com>
2022-11-12 16:02:01 +0530

    set and save table data

--- src/com/opk/pos/controller/CustomerFormController.java
+++ src/com/opk/pos/controller/CustomerFormController.java
@@ -2,15 +2,19 @@ package com.opk.pos.controller;
 
 import com.opk.pos.db.Database;
 import com.opk.pos.model.Customer;
+import com.opk.pos.view.tm.CustomerTM;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CustomerFormController {
     public AnchorPane customerFormContext;
@@ -18,6 +22,21 @@ public class CustomerFormController {
     public TextField txtName;
     public TextField txtAddress;
     public TextField txtSalary;
+    public TableColumn colCustomerId;
+    public TableColumn colCustomerName;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+    public TableColumn colOption;
+    public TableView<CustomerTM> tblCustomer;
+
+    public void initialize(){
+        setTableData();
+        colCustomerId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colCustomerName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        colOption.setCellValueFactory(new PropertyValueFactory<>("btn"));
+    }
 
     public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
         setUi("DashBoardForm","Dashboard");
@@ -38,8 +57,22 @@ public class CustomerFormController {
         );
         if (Database.customerTable.add(customer)){
             new Alert(Alert.AlertType.CONFIRMATION,"Customer Saved!").show();
+            setTableData();
         }else{
             new Alert(Alert.AlertType.CONFIRMATION,"Try Again!").show();
         }
     }
+
+    private void setTableData(){
+        ArrayList<Customer> customerList=Database.customerTable;
+        ObservableList<CustomerTM> obList= FXCollections.observableArrayList();
+        for (Customer c:customerList
+             ) {
+            Button btn = new Button("Delete");
+            CustomerTM tm = new CustomerTM(c.getId(),c.getName(),c.getAddress(),c.getSalary(),btn);
+            obList.add(tm);
+        }
+        tblCustomer.setItems(obList);
+    }
+
 }
