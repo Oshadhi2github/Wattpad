d72e8cab75f02e920dc9cebb2e283bef6d9fee8d
Oshadhi2github <pabasarasgck@gmail.com>
2022-11-12 12:12:18 +0530

    customer management navigation part

--- src/com/opk/pos/controller/DashBoardFormController.java
+++ src/com/opk/pos/controller/DashBoardFormController.java
@@ -3,9 +3,15 @@ package com.opk.pos.controller;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -16,6 +22,7 @@ import java.util.Date;
 public class DashBoardFormController {
     public Label lblDate;
     public Label lblTime;
+    public AnchorPane dashboardContext;
 
     public void initialize(){
         setDateAndTime();
@@ -43,5 +50,27 @@ public class DashBoardFormController {
         timeline.play();
 
 
+    }
+
+    public void openPlaceOrderFormOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void openProductManagementOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void openStatisticsOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void openCustomerFormOnAction(MouseEvent mouseEvent) throws IOException {
+        setUi("CustomerForm","Customer Form");
+    }
+
+    public void openOrderDetailsOnAction(MouseEvent mouseEvent) {
+    }
+
+    private void setUi(String location, String title) throws IOException {
+        Stage window = (Stage)dashboardContext.getScene().getWindow();
+        window.setTitle(title);
+        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
     }
 }
