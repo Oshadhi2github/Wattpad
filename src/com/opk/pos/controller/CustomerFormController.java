9d8a48858517eece9400f67af3e4fa22efb0fd9c
Oshadhi2github <pabasarasgck@gmail.com>
2022-11-16 00:18:52 +0530

    updated fields clear and change name in update customer as save customer.

--- src/com/opk/pos/controller/CustomerFormController.java
+++ src/com/opk/pos/controller/CustomerFormController.java
@@ -1,5 +1,6 @@
 package com.opk.pos.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.opk.pos.db.Database;
 import com.opk.pos.model.Customer;
 import com.opk.pos.view.tm.CustomerTM;
@@ -28,8 +29,9 @@ public class CustomerFormController {
     public TableColumn colSalary;
     public TableColumn colOption;
     public TableView<CustomerTM> tblCustomer;
+    public JFXButton btnSaveUpdate;
 
-    public void initialize(){
+    public void initialize() {
         setTableData();
         setColCustomerId();
         colCustomerId.setCellValueFactory(new PropertyValueFactory<>("id"));
@@ -37,70 +39,119 @@ public class CustomerFormController {
         colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
         colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
         colOption.setCellValueFactory(new PropertyValueFactory<>("btn"));
+
+        //========listeners=========
+
+        tblCustomer.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (null != newValue) {
+                setCustomerData(newValue);
+            }
+        });
+        //========listeners=========
+    }
+
+    private void setCustomerData(CustomerTM tm) {
+        txtId.setText(tm.getId());
+        txtName.setText(tm.getName());
+        txtAddress.setText(tm.getAddress());
+        txtSalary.setText(String.valueOf(tm.getSalary()));
+        btnSaveUpdate.setText("Update Customer");
     }
 
     public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
-        setUi("DashBoardForm","Dashboard");
+        setUi("DashBoardForm", "Dashboard");
     }
 
     private void setUi(String location, String title) throws IOException {
-        Stage window = (Stage)customerFormContext.getScene().getWindow();
+        Stage window = (Stage) customerFormContext.getScene().getWindow();
         window.setTitle(title);
-        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"))));
     }
 
     public void saveUpdateOnAction(ActionEvent actionEvent) {
+
         Customer customer = new Customer(
                 txtId.getText(),
                 txtName.getText(),
                 txtAddress.getText(),
                 Double.parseDouble(txtSalary.getText())
         );
-        if (Database.customerTable.add(customer)){
-            new Alert(Alert.AlertType.CONFIRMATION,"Customer Saved!").show();
-            setTableData();
-            setColCustomerId();
-        }else{
-            new Alert(Alert.AlertType.CONFIRMATION,"Try Again!").show();
-        }
-    }
-
-    private void setTableData(){
-        ArrayList<Customer> customerList=Database.customerTable;
-        ObservableList<CustomerTM> obList= FXCollections.observableArrayList();
-        for (Customer c:customerList
-             ) {
-            Button btn = new Button("Delete");
-            CustomerTM tm = new CustomerTM(c.getId(),c.getName(),c.getAddress(),c.getSalary(),btn);
-            obList.add(tm);
-        }
-        tblCustomer.setItems(obList);
-    }
-
-    private void setColCustomerId(){
-        //get last saved customer
-        //catch the id(C-001)
-        //separate the numbers from the character
-        //increment the separated number
-        //concat the character again to the incremented number(C-002)
-        //set CustomerId
-        if(!Database.customerTable.isEmpty()){
-            Customer c=Database.customerTable.get(Database.customerTable.size()-1);// [10,20] size=2 => last => size-1 = 1 [10,20]
-            String id = c.getId();//c-002
-            String dataArray[] = id.split("-");// =>["C","002"];//java string class => split
-            id=dataArray[1]; //002
-            int oldNumber= Integer.parseInt(id); // 2 => 00 remove
-            oldNumber++; // 3
-            if (oldNumber<9){
-                txtId.setText("C-00"+oldNumber);
-            }else if (oldNumber<99){
-                txtId.setText("C-0"+oldNumber);
-            }else{
-                txtId.setText("C-"+oldNumber);
+        if (btnSaveUpdate.getText().equalsIgnoreCase("Save Customer")) {
+            //save
+            if (Database.customerTable.add(customer)) {
+                new Alert(Alert.AlertType.CONFIRMATION, "Customer Saved!").show();
+                setTableData();
+                setColCustomerId();
+                clear();
+            } else {
+                new Alert(Alert.AlertType.CONFIRMATION, "Try Again!").show();
             }
         }else{
-            txtId.setText("C-001");
+            for (Customer c : Database.customerTable) {
+                if (txtId.getText().equalsIgnoreCase(c.getId())) {
+                    c.setName(txtName.getText());
+                    c.setAddress(txtAddress.getText());
+                    c.setSalary(Double.parseDouble(txtSalary.getText()));
+                    new Alert(Alert.AlertType.CONFIRMATION, "Customer Updated!").show();
+                    setTableData();
+                    clear();
+                }
+            }
         }
-    }
 
+    }
+        private void clear() {
+            btnSaveUpdate.setText("Save Customer");
+
+            txtName.clear();
+            txtAddress.setText("");
+            txtSalary.clear();
+            setColCustomerId();
+        }
+
+
+        private void setTableData() {
+            ArrayList<Customer> customerList = Database.customerTable;
+            ObservableList<CustomerTM> obList = FXCollections.observableArrayList();
+            for (Customer c : customerList
+            ) {
+                Button btn = new Button("Delete");
+                CustomerTM tm = new CustomerTM(c.getId(), c.getName(), c.getAddress(), c.getSalary(), btn);
+                obList.add(tm);
+            }
+            tblCustomer.setItems(obList);
+        }
+
+        private void setColCustomerId () {
+            //get last saved customer
+            //catch the id(C-001)
+            //separate the numbers from the character
+            //increment the separated number
+            //concat the character again to the incremented number(C-002)
+            //set CustomerId
+            if (!Database.customerTable.isEmpty()) {
+                Customer c = Database.customerTable.get(Database.customerTable.size() - 1);// [10,20] size=2 => last => size-1 = 1 [10,20]
+                String id = c.getId();//c-002
+                String dataArray[] = id.split("-");// =>["C","002"];//java string class => split
+                id = dataArray[1]; //002
+                int oldNumber = Integer.parseInt(id); // 2 => 00 remove
+                oldNumber++; // 3
+                if (oldNumber < 9) {
+                    txtId.setText("C-00" + oldNumber);
+                } else if (oldNumber < 99) {
+                    txtId.setText("C-0" + oldNumber);
+                } else {
+                    txtId.setText("C-" + oldNumber);
+                }
+            } else {
+                txtId.setText("C-001");
+            }
+        }
+
+
+    public void newCustomerOnAction(ActionEvent actionEvent) {
+    clear();
+    }
 }
+
+
