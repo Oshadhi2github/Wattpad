bfe31f22a09019f32a2081079a9441c14f5264f1
Oshadhi2github <pabasarasgck@gmail.com>
2022-11-17 12:28:21 +0530

    add customers and products using static block in database

--- src/com/opk/pos/db/Database.java
+++ src/com/opk/pos/db/Database.java
@@ -10,4 +10,16 @@ public class Database {
     public static ArrayList<User> userTable = new ArrayList<User>();
     public static ArrayList<Customer> customerTable = new ArrayList<Customer>();
     public static ArrayList<Product> productTable = new ArrayList<Product>();
+
+    static {
+        customerTable.add(new Customer("C-001","Anne","Colombo",45000));
+        customerTable.add(new Customer("C-002","Fathima","Kegalle",50000));
+        customerTable.add(new Customer("C-003","Krish","Kandy",40000));
+
+        productTable.add(new Product("D-001","Description 1",25,4500));
+        productTable.add(new Product("D-002","Description 2",90,450));
+        productTable.add(new Product("D-003","Description 3",15,750));
+        productTable.add(new Product("D-004","Description 4",50,500));
+        productTable.add(new Product("D-005","Description 5",45,600));
+    }
 }
